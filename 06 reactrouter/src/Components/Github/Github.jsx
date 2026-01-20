58388615e7b6cb64a3c2143d3ec450272aaf4b67
Mayur Bhuran <bhuranmayur20@gmail.com>
2026-01-20 22:54:41 +0530

    this is my new react router project

---
+++ 06 reactrouter/src/Components/Github/Github.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect,useState } from 'react'
+
+function Github() {
+  const [data,setdata]=useState([]);
+  useEffect(() => {
+    fetch('https://api.github.com/users/MayurBhuran')
+    .then((response => response.json()))
+    .then(data => console.log(data))
+    setdata(data);
+  }, [])
+  return (
+    <div className='text-center m-4 bg-gray-600
+     text-white p-4 text-3xl'>Github Follwers:</div>
+  )
+}
+
+export default Github
---
+++ 06 reactrouter/src/Layout.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './Components/Header/header'
+import Footer from './Components/Footer/Footer'
+import { Outlet } from 'react-router-dom'
+import About from './Components/About/About'
+
+function Layout() {
+  return (
+   <> 
+   <Header />
+   <Outlet />
+   <Footer />
+   <About />
+
+   </>
+  )
+}
+
+export default Layout
