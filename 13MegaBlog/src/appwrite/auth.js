61afd8ff722a6b881d9fdd07a30a3467f61c2739
Mayur Bhuran <bhuranmayur20@gmail.com>
2026-02-09 20:18:35 +0530

    today build authentication service with appwrite

---
+++ 13MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,60 @@
+import conf from "../Conf.js";
+import { Client, Account, ID } from "appwrite";
+
+
+export class AuthService {
+  Client = new Client();
+  Account;
+
+  constructor() {
+    this.Client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.Account = new Account(this.Client);
+  }
+
+  async createAccount(email, password, name) {
+    try {
+      const userAccount = await this.Account.create(ID.unique(), email, password, name);
+      if (userAccount) {
+        return this.login(email, password);
+      }
+      else {
+        return null;
+      }
+
+    } catch (error) {
+      throw error;
+
+    }
+  }
+  async login(email, password) {
+    try {
+      const session = await this.account.createEmailSession(email, password);
+      return session;
+    } catch (error) {
+      throw error;
+    }
+}
+async currentUser() { 
+  try {
+    return await this.Account.get();
+    
+  } catch (error) {
+    console.log("Appwrite service :: currentUser :: error ", error);
+ 
+  } 
+  return null;
+  }
+async logout() {
+  try {
+    await this.account.deleteSession();
+  } catch (error) {
+    console.log("Appwrite service :: logout :: error ", error);
+  }
+}
+}
+
+const authService = new AuthService();
+
+export default authService
