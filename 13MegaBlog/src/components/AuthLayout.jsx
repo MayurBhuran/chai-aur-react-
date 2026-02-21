bd65d84a028075e70887dac7be1d21e026388545
Mayur Bhuran <bhuranmayur20@gmail.com>
2026-02-21 23:06:50 +0530

    using React hook form in production

---
+++ 13MegaBlog/src/components/AuthLayout.jsx
@@ -0,0 +1,26 @@
+import React, {useEffect,useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+export default function Protected({ children,authentication =true })
+ {
+  const navigate = useNavigate();
+  const [loading,setLoading] = useState(true)
+  const authStatus = useSelector((state) => state.auth.status);
+
+  useEffect(() => {
+    
+    if (authentication && authStatus === false) {
+      navigate("/login");
+    } else if (!authentication && authStatus === true) {
+      navigate("/");
+    }
+    setLoading(false)
+  }, [authStatus, authentication, navigate]);
+
+  return (
+    <div>AuthLayout</div>
+  )
+}
+
+
